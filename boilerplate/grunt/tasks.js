8c24e8c4d394a50f4bb34e9032db63f72559a86b
Aaron Lord <aaronlord1@gmail.com>
2014-07-31 16:25:59 +0100

    Add forgotten grunt modernizr to tasks

--- boilerplate/grunt/tasks.js
+++ boilerplate/grunt/tasks.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
 
     grunt.registerTask('build', ['sass']);
 
-    grunt.registerTask('release', ['sprite', 'build', 'requirejs', 'uglify']);
+    grunt.registerTask('release', ['sprite', 'build', 'modernizr', 'requirejs', 'uglify']);
 
     grunt.registerTask('default', ['build', 'watch']);
     
